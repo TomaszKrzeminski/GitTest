efd37df90ae2e98a062f62d6bddb56f90f9e71d3
Tomasz Krzeminski <koral2323@gmail.com>
2021-07-05 23:08:55 +0200

    Messanger1

---
+++ WebApplication1/WebApplication1/Controllers/MessangerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MessangerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
